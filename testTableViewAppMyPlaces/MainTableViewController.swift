96c1419d79a8f53bd20494bd74db4b2016573e5d
ziman <alex.cheranov@yandex.ru>
2021-07-13 13:27:31 +0500

    first commit: image and text label was added

---
+++ testTableViewAppMyPlaces/MainTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainTableViewController.swift
+//  testTableViewAppMyPlaces
+//
+//  Created by Алексей Черанёв on 13.07.2021.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+
+    let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return restaurantNames.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        return cell
+    }
+
+}
