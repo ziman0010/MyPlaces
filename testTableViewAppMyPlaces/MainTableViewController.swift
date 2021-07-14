c86e17c7cd6f9ee6bf9a5624ba93ad337e6faed5
ziman <alex.cheranov@yandex.ru>
2021-07-14 12:43:59 +0500

    Images was rounded, height was edited

--- testTableViewAppMyPlaces/MainTableViewController.swift
+++ testTableViewAppMyPlaces/MainTableViewController.swift
@@ -31,7 +31,13 @@ class MainTableViewController: UITableViewController {
         
         cell.textLabel?.text = restaurantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         return cell
+    }
+    //MARK: - Table view Delegate
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
 
 }
