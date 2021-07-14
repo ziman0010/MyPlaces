7dc1cbd1a560cec403f4975f55d8ab9464259463
ziman <alex.cheranov@yandex.ru>
2021-07-14 13:20:10 +0500

    Place Model 1

--- testTableViewAppMyPlaces/MainTableViewController.swift
+++ testTableViewAppMyPlaces/MainTableViewController.swift
@@ -8,13 +8,8 @@
 import UIKit
 
 class MainTableViewController: UITableViewController {
-
-    let restaurantNames = [
-        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
-        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
-        "Speak Easy", "Morris Pub", "Вкусные истории",
-        "Классик", "Love&Life", "Шок", "Бочка"
-    ]
+    
+    let places = Place.getPlaces()
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -23,21 +18,19 @@ class MainTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return restaurantNames.count
+        return places.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         
-        cell.nameLabel.text = restaurantNames[indexPath.row]
-        cell.imageOfPlace.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.nameLabel.text = places[indexPath.row].name
+        cell.locationLabel.text = places[indexPath.row].location
+        cell.typeLabel.text = places[indexPath.row].type
+        cell.imageOfPlace.image = UIImage(named: places[indexPath.row].image)
         cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.height / 2
         cell.imageOfPlace.clipsToBounds = true
         return cell
-    }
-    //MARK: - Table view Delegate
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 85
     }
 
 }
---
+++ testTableViewAppMyPlaces/PlaceModel.swift
@@ -0,0 +1,31 @@
+//
+//  PlaceModel.swift
+//  testTableViewAppMyPlaces
+//
+//  Created by Алексей Черанёв on 14.07.2021.
+//
+
+import Foundation
+
+struct Place {
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    static let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
+    static func getPlaces() -> [Place] {
+        var places = [Place]()
+        
+        for place in restaurantNames
+        {
+            places.append(Place(name: place, location: "Уфа", type: "Ресторан", image: place))
+        }
+        return places
+    }
+}
