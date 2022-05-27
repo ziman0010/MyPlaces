95472756af0da1690be113f2d1bac50d740a5eaf
ziman <alex.cheranov@yandex.ru>
2022-05-27 13:26:48 +0300

    test2

---
+++ testTableViewAppMyPlaces/StorageManager — копия 2.swift
@@ -0,0 +1,25 @@
+//
+//  StorageManager.swift
+//  testTableViewAppMyPlaces
+//
+//  Created by Алексей Черанёв on 15.07.2021.
+//
+
+import RealmSwift
+
+let realm =  try! Realm()
+
+class StorageManager {
+    
+    static func saveObject(_ place: Place) {
+        try! realm.write {
+            realm.add(place)
+        }
+    }
+    
+    static func deleteObject(_ place: Place) {
+        try! realm.write {
+            realm.delete(place)
+        }
+    }
+}
